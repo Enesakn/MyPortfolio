c8b3a0a8e112f50da97c9c17721623963cfe4be8
Muhammet Enes Akın <akine7160@gmail.com>
2025-04-27 21:08:11 +0300

    theme added, ViewComponents started to be created

--- MyPortfolio/Program.cs
+++ MyPortfolio/Program.cs
@@ -8,10 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MyPortfolioDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
-
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Default}/{action=Index}/{id?}"
+);
 
 app.Run();
