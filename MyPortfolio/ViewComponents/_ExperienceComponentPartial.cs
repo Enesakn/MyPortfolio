c0957ea4f5361520316ac231236d9d64415acb38
Muhammet Enes Akın <akine7160@gmail.com>
2025-04-28 23:37:00 +0300

    incomplete listings completed

--- MyPortfolio/ViewComponents/_ExperienceComponentPartial.cs
+++ MyPortfolio/ViewComponents/_ExperienceComponentPartial.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _ExperienceComponentPartial:ViewComponent
     {
+        private readonly MyPortfolioDbContext _myPortfolioDbContext;
+        public _ExperienceComponentPartial(MyPortfolioDbContext myPortfolioDbContext)
+        {
+            _myPortfolioDbContext = myPortfolioDbContext;
+        }
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = _myPortfolioDbContext.Experiences.ToList();
+            return View(values);
         }
     }
 }
--- MyPortfolio/ViewComponents/_SkillComponentPartial.cs
+++ MyPortfolio/ViewComponents/_SkillComponentPartial.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _SkillComponentPartial : ViewComponent
     {
+        private readonly MyPortfolioDbContext _myPortfolioDbContext;
+        public _SkillComponentPartial(MyPortfolioDbContext myPortfolioDbContext)
+        {
+            _myPortfolioDbContext = myPortfolioDbContext;
+        }
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = _myPortfolioDbContext.Skills.ToList();
+            return View(values);
         }
     }
 }
