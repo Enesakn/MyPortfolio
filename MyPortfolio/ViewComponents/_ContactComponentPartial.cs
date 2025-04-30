1ae9f7db4dc026996e30896e68dadecb8718b750
Muhammet Enes Akın <akine7160@gmail.com>
2025-04-30 16:23:00 +0300

    I logged into the admin panel and integrated it with RenderBody

--- MyPortfolio/ViewComponents/_ContactComponentPartial.cs
+++ MyPortfolio/ViewComponents/_ContactComponentPartial.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _ContactComponentPartial:ViewComponent
     {
+        private readonly MyPortfolioDbContext _myPortfolioDbContext;
+        public _ContactComponentPartial(MyPortfolioDbContext myPortfolioDbContext)
+        {
+            _myPortfolioDbContext = myPortfolioDbContext;
+        }
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values=_myPortfolioDbContext.Contacts.ToList();
+            var contactInfo = _myPortfolioDbContext.Contacts.FirstOrDefault();
+            ViewBag.Description = contactInfo.Description;
+            ViewBag.Tittle = contactInfo.Tittle;
+            return View(values);
         }
     }
 }
